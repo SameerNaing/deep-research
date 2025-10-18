23e9fc51d0042b350e9ea3e38151ab0ded82243f
Naing Ye Oo <samnaing.ucsmub@gmail.com>
2025-10-18 09:35:27 +0400

    init proj

---
+++ be/deepresearch/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for deepresearch project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deepresearch.settings')
+
+application = get_wsgi_application()
